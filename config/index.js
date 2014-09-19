e4cbc3c8ef1aab3824fa02ffdbfdf67cc91c47c9
Gerardo <gerardo.prieto@olx.com>
2014-09-19 16:00:38 -0300

    config

--- config/index.js
+++ config/index.js
@@ -19,7 +19,7 @@ module.exports = {
         },
         html5: {
                 desiredCapabilities: {
-                browserName : 'firefox' ,
+                browserName : 'phantomjs' ,
                 logLevel: 'silent',
                 'phantomjs.page.customHeaders.User-Agent' : 'Mozilla/5.0 (Linux; U; Android 4.1; xx-xx; GT-I9082 Build/JZO54K) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30'
                 }
