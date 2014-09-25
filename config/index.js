584935d75999f1eb4ca53b324b86af13eabf5c4c
Gerardo <gerardo.prieto@olx.com>
2014-09-25 16:47:39 -0300

    more changes

--- config/index.js
+++ config/index.js
@@ -8,15 +8,15 @@ module.exports = {
                 browserName : 'phantomjs' ,
                 logLevel: 'silent',
                 'phantomjs.page.customHeaders.User-Agent' : 'WAP',
-                'phantomjs.page.customHeaders.x-origin-olx' : 'testing'
+         //       'phantomjs.page.customHeaders.x-origin-olx' : 'testing'
                 }
         },
         html4: {
                 desiredCapabilities: {
-                browserName : 'chrome' ,
+                browserName : 'phantomjs' ,
                 logLevel: 'silent',
                 'phantomjs.page.customHeaders.User-Agent' : 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7A341 Safari/528.16',
-                'phantomjs.page.customHeaders.x-origin-olx' : 'testing'
+              //  'phantomjs.page.customHeaders.x-origin-olx' : 'testing'
                 }
         },
         html5: {
@@ -24,7 +24,7 @@ module.exports = {
                 browserName : 'phantomjs' ,
                 logLevel: 'silent',
                 'phantomjs.page.customHeaders.User-Agent' : 'Mozilla/5.0 (Linux; U; Android 4.1; xx-xx; GT-I9082 Build/JZO54K) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30',
-                'phantomjs.page.customHeaders.x-origin-olx' : 'testing'
+             //   'phantomjs.page.customHeaders.x-origin-olx' : 'testing'
                 }
         }
     }
